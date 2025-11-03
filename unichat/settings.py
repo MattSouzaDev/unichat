2286d54a7a9e4ecf7fe25016c9c38d8ccd645cd9
MattSouzaDev <souzabusinessmatheus@gmail.com>
2025-11-03 17:18:41 -0400

    configurando settings

--- unichat/settings.py
+++ unichat/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get(
     "SECRET_KEY", "django-insecure-9df+igxnu!9f62$u09e$i4*x)b92x344bhq(b-8p=4ag4@@5+*"
 )
-DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
+DEBUG = False
 
 # Railway injects the deployment domain automatically; add localhost for dev
 ALLOWED_HOSTS = ['unichat-prod.up.railway.app','localhost', '127.0.0.1']
@@ -86,7 +86,7 @@ CHANNEL_LAYERS = {
 DATABASE_URL = os.environ.get("DATABASE_URL")
 if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
     }
 else:
     DATABASES = {
